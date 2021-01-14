05800fa93b89ee13517f8b31741beb26b71db6b4
jaeel29 <elferkhjaber@gmail.com>
2021-01-14 11:37:52 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Buy coffee', key: '1' },
+    { text: 'Design something', key: '2' },
+    { text: 'Play football', key: '3' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +29,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    backgroundColor: '#fff05a',
+    padding: 15,
+    marginTop: 10,
   },
 });
